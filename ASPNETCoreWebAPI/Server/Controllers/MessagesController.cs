2c4f6d952a8aaa59f0452c345573f9651e3c0611
Truong <vuanh.truong@saxsys.de>
2020-01-20 14:52:41 +0100

    Added Web-API Documentation

--- ASPNETCoreWebAPI/Server/Controllers/MessagesController.cs
+++ ASPNETCoreWebAPI/Server/Controllers/MessagesController.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using Server.Database;
 using SharedLibrary;
 
